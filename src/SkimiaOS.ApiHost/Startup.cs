339229ecf7587c87e55f410ded377e3c4f938048
Kessler <kessler.dev@gmail.com>
2017-03-01 00:24:33 +0100

    Add Loggin with Serilog in logs/* dir

--- src/SkimiaOS.ApiHost/Startup.cs
+++ src/SkimiaOS.ApiHost/Startup.cs
@@ -6,84 +6,85 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
+using Serilog;
 using Skimia.Extensions.Discovery.Attributes;
 using Skimia.Extensions.Discovery.Attributes.Abstractions;
 using Skimia.Extensions.Messages;
 
-namespace SkimiaOS.ApiHost
-{
-    public class Startup : ExtCore.WebApplication.Startup
-    {
-        public Startup(IHostingEnvironment env, IServiceProvider serviceProvider) 
-            : base(serviceProvider)
-        {
-            
-            var builder = new ConfigurationBuilder()
-                .SetBasePath(env.ContentRootPath)
-                .AddJsonFile("config/framework.json", optional: true, reloadOnChange: true)
-                .AddJsonFile("config/plugins.json", optional: true, reloadOnChange: true)
-                .AddJsonFile($"config/framework.{env.EnvironmentName}.json", optional: true)
-                .AddEnvironmentVariables();
+namespace SkimiaOS.ApiHost {
+    public class Startup : ExtCore.WebApplication.Startup {
+        public Startup (IHostingEnvironment env, IServiceProvider serviceProvider): base (serviceProvider) {
+                
+            var builder = new ConfigurationBuilder ()
+                .SetBasePath (env.ContentRootPath)
+                .AddJsonFile ("config/framework.json", optional : true, reloadOnChange : true)
+                .AddJsonFile ("config/serilog.json", optional : true, reloadOnChange : true)
+                .AddJsonFile ("config/plugins.json", optional : true, reloadOnChange : true)
+                .AddJsonFile ($"config/framework.{env.EnvironmentName}.json", optional : true)
+                .AddEnvironmentVariables ();
 
-            configurationRoot = builder.Build();
+            configurationRoot = builder.Build ();
 
-            this.serviceProvider.GetService<ILoggerFactory>().AddConsole(configurationRoot.GetSection("Logging"));
+            Log.Logger = CreateLogger();
+
+            this.serviceProvider.GetService<ILoggerFactory> ()
+                .AddConsole (configurationRoot.GetSection ("Logging"))
+                .AddSerilog();
         }
 
         // This method gets called by the runtime. Use this method to add services to the container.
-        public override void ConfigureServices(IServiceCollection services)
-        {
+        public override void ConfigureServices (IServiceCollection services) {
             // Add framework services.
-            services.AddMvc();
-            services.AddCors(options =>
-            {
-                options.AddPolicy("AllowAnyOriginHeadersAndMethods",
-                    builder =>
-                    {
-                        builder.AllowAnyOrigin()
-                            .AllowAnyHeader()
-                            .AllowAnyMethod()
-                            .AllowCredentials();
+            services.AddMvc ();
+            services.AddCors (options => {
+                options.AddPolicy ("AllowAnyOriginHeadersAndMethods",
+                    builder => {
+                        builder.AllowAnyOrigin ()
+                            .AllowAnyHeader ()
+                            .AllowAnyMethod ()
+                            .AllowCredentials ();
                     }
                 );
             });
-            services.AddDiscovery();
-            services.AddMessages();
+            services.AddDiscovery ();
+            services.AddMessages ();
 
-            base.ConfigureServices(services);
+            base.ConfigureServices (services);
 
             //ensure maindbcontext from ExtCore
-            var s = ExtensionManager.GetInstance<Storage>();
-            services.AddSingleton(s.StorageContext);
+            var s = ExtensionManager.GetInstance<Storage> ();
+            services.AddSingleton (s.StorageContext);
 
-            RebuildServiceProvider(services);
+            RebuildServiceProvider (services);
             //sure db is OK!
-            s.StorageContext.Database.EnsureCreated();
+            s.StorageContext.Database.EnsureCreated ();
 
-            LoadExtensionInDiscover();
+            LoadExtensionInDiscover ();
 
-            
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public override void Configure(IApplicationBuilder app)
-        {
-            base.Configure(app);
+        public override void Configure (IApplicationBuilder app) {
+            base.Configure (app);
             //can be mloved in another pluggin
-            app.UseMvc();
+            app.UseMvc ();
         }
 
-        private void RebuildServiceProvider(IServiceCollection services)
+        private Serilog.ILogger CreateLogger()
         {
-            serviceProvider = services.BuildServiceProvider();
+            return new LoggerConfiguration ()
+                .ReadFrom.ConfigurationSection(configurationRoot.GetSection ("Serilog"))
+                .CreateLogger ();
+        }
+        private void RebuildServiceProvider (IServiceCollection services) {
+            serviceProvider = services.BuildServiceProvider ();
             foreach (IExtension extension in ExtensionManager.Extensions)
-                extension.SetServiceProvider(serviceProvider);
+            extension.SetServiceProvider (serviceProvider);
         }
 
-        private void LoadExtensionInDiscover()
-        {
-            var discover = serviceProvider.GetRequiredService<IAttributeDiscover>();
-            discover.RegisterAssemblies(ExtensionManager.Assemblies);
+        private void LoadExtensionInDiscover () {
+            var discover = serviceProvider.GetRequiredService<IAttributeDiscover> ();
+            discover.RegisterAssemblies (ExtensionManager.Assemblies);
         }
     }
 }
